bee91bc51c1ce68fb61bb0be14e7a8ab9dbfd412
leonlux <leonlux@icloud.com>
2023-11-21 15:25:31 +0100

    add junit to dependencies and generate test stubs

---
+++ src/test/java/SparseVectorTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SparseVectorTest {
+
+    @Test
+    void setElement() {
+    }
+
+    @Test
+    void getElement() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+}
