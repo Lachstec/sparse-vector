665bcd83be07785837e895e2471d6048cec57304
Max Cherris <MCherris@protonmail.com>
2023-11-21 13:55:17 +0100

    added javadoc for getElement

--- src/main/java/ISparseVector.java
+++ src/main/java/ISparseVector.java
@@ -6,6 +6,12 @@ public interface ISparseVector {
      * @param value, of the element that is to be placed
      */
     void setElement(int index, double value);
+
+    /**
+     * Finds the element at the specified index
+     * @param index, the index of the element
+     * @return returns the value of the element
+     */
     double getElement(int index);
     void remove(int index);
     void add(ISparseVector vector);
