47d8999032cd002158f69162b78e502a816d369c
Max Cherris <MCherris@protonmail.com>
2023-11-21 13:52:12 +0100

    added javadoc for getElement

--- src/main/java/ISparseVector.java
+++ src/main/java/ISparseVector.java
@@ -1,4 +1,10 @@
 public interface ISparseVector {
+
+    /**
+     * Inserts the value at the index specified.
+     * @param index, where the element is to be placed
+     * @param value, of the element that is to be placed
+     */
     void setElement(int index, double value);
     double getElement(int index);
     void remove(int index);
