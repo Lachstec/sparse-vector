6d7b1c514ca2dade0bce18d9c0fde713e5d5c85e
leonlux <leonlux@icloud.com>
2023-11-21 14:06:49 +0100

    add template for SparseVector

---
+++ src/main/java/SparseVector.java
@@ -0,0 +1,27 @@
+public class SparseVector implements ISparseVector {
+
+    @Override
+    public void setElement(int index, double value) {
+
+    }
+
+    @Override
+    public double getElement(int index) {
+        return 0;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void add(ISparseVector vector) {
+
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+}
